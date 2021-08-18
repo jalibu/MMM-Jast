33d0b40b66df35116c4f9e04663ac8ef0756d7a4
Jan Litzenburger <jan.litzenburger@sap.com>
2021-08-18 17:16:45 +0200

    removed dependabot alert

--- @types/Module/index.d.ts
+++ @types/Module/index.d.ts
@@ -2,16 +2,20 @@ declare module Module {
   export function register(
     moduleName: string,
     moduleProperties: {
-      defaults: any;
-      getStyles: Function;
-      getTranslations: Function;
-      getTemplate: Function;
-      getTemplateData: Function;
-      start: Function;
-      scheduleUpdate: Function;
-      loadData: Function;
-      socketNotificationReceived: Function;
+      data?: any;
+      defaults?: any;
+      getDom?: Function;
       getHeader?: Function;
+      getScripts?: Function;
+      getStyles?: Function;
+      getTemplate?: Function;
+      getTemplateData?: Function;
+      getTranslations?: Function;
+      loadData?: Function;
+      notificationReceived?: Function;
+      scheduleUpdate?: Function;
+      socketNotificationReceived?: Function;
+      start?: Function;
     }
   ): void;
 }
