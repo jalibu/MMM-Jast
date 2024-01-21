3b08d5df8276584927257542c0f77ec09661c7e2
Jan Litzenburger <jan.litzenburger@sap.com>
2024-01-21 12:38:15 +0100

    CSS improvements

--- MMM-Jast.js
+++ MMM-Jast.js
@@ -1,6 +1,6 @@
 /*! *****************************************************************************
   mmm-jast
-  Version 2.9.1
+  Version 2.9.2
 
   A minimalistic stock ticker based on Yahoo's finance API for the MagicMirror² platform.
   Please submit bugs at https://github.com/jalibu/MMM-Jast/issues
